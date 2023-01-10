f7035b1b1c8a350cbb8cdaff42a060049dd23303
Ankit Prakash <prakashankit526@gmail.com>
2023-01-10 11:26:44 +0530

    added password generator homepage

---
+++ password-generator/src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+const Home = () => {
+    return (
+        <div className={styles.body}>
+            <div className={styles.title}>
+                Password Generator
+            </div>
+            <div className={styles.password}>
+
+            </div>
+            <div className={styles.container}>
+
+            </div>
+        </div>
+    );
+};
+
+export default Home;
